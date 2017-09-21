a9f8d074b0ebeacfe79aeeab33c6e65111c03771
Joey Smoll <jsmoll91@gmail.com>
2017-09-21 04:04:45 +0000

    7 checkpoint submission complete

--- lib/messaging.rb
+++ lib/messaging.rb
@@ -14,4 +14,10 @@ module Messaging
         message_data = {body: {sender: sender_email, recipient_id: recipient_id, token: token, subject: message_subject, stripped: message_body}, headers: { "authorization" => @auth_token }}    
         self.class.post(base_api_endpoint("messages"), message_data)
     end
+    
+    def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+        submission = {body: {checkpoint_id: checkpoint_id, assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, comment: comment, enrollment_id: 28986}, headers: { "authorization" => @auth_token }}
+        self.class.post(base_api_endpoint("checkpoint_submissions"), submission)
+    end
+    
 end
